483db6b6679e4806622fcf837645f300f72a7c80
Brenton Paulsen <brenton.paulsen@hotmail.com>
2015-12-25 18:41:50 -0800

    Cleaned up source code  and removed unimplemented test project

--- InitializeFrom/InitializeFrom/CodeFixProvider.cs
+++ InitializeFrom/InitializeFrom/CodeFixProvider.cs
@@ -32,7 +32,6 @@ namespace InitializeFrom
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
@@ -64,22 +63,6 @@ namespace InitializeFrom
             var newRoot = root.ReplaceNode(initializer, newInit);
 
             return document.WithSyntaxRoot(newRoot);
-
-
-            //var identifierToken = typeDecl.Identifier;
-            //var newName = identifierToken.Text.ToUpperInvariant();
-
-            //// Get the symbol representing the type to be renamed.
-            //var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            //var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
-
-            //// Produce a new solution that has all references to that type renamed, including the declaration.
-            //var originalSolution = document.Project.Solution;
-            //var optionSet = originalSolution.Workspace.Options;
-            //var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
-
-            //// Return the new solution with the now-uppercase type name.
-            //return newSolution;
         }
     }
 }
